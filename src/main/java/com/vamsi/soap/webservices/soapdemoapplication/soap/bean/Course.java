076e8f72ded781e73ac9fc691c9f2283453cc48a
vamsikrm <vamsikrm@VACDJG0IZX.asiapacific.hpqcorp.net>
2018-04-24 09:05:44 +0530

    commit for security

--- src/main/java/com/vamsi/soap/webservices/soapdemoapplication/soap/bean/Course.java
+++ src/main/java/com/vamsi/soap/webservices/soapdemoapplication/soap/bean/Course.java
@@ -1,7 +1,12 @@
 package com.vamsi.soap.webservices.soapdemoapplication.soap.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Course {
 
+	@Id
 	private int id;
 	private String name;
 	private String description;
